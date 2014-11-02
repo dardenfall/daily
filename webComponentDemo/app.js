823a75bfa8d5299b9d3f8edf121c6e4eaaf00363
Anthony Scavone <ascavone@mediamath.com>
2014-11-02 11:19:17 -0500

    got 2nd example working

--- webComponentDemo/app.js
+++ webComponentDemo/app.js
@@ -11,5 +11,13 @@ app.engine('html', hbs.__express);
 app.get('/step1', function(req, res) {
    res.render('polymer-tutorial-master/step-1/index');
 });
+
+app.get('/step2', function(req, res) {
+   res.render('polymer-tutorial-master/step-2/index');
+});
+
+app.get('/post-card.html', function(req, res) {
+   res.render('polymer-tutorial-master/step-2/post-card.html');
+});
  
 app.listen(3001);
