23b2532462d19a9b297f8776e3bf96a9b7a868ef
Anthony Scavone <ascavone@mediamath.com>
2014-12-18 08:25:43 -0500

    Trying to start timer web component

--- webComponentDemo/app.js
+++ webComponentDemo/app.js
@@ -1,3 +1,6 @@
+// to run:  C:\sandbox\daily\webComponentDemo>node app.js
+// browser: http://localhost:3001/step2 (step#)
+
 var express = require('express');
 var app = express();
  
@@ -19,5 +22,25 @@ app.get('/step2', function(req, res) {
 app.get('/post-card.html', function(req, res) {
    res.render('polymer-tutorial-master/step-2/post-card.html');
 });
- 
+
+app.get('/step3', function(req, res) {
+   res.render('polymer-tutorial-master/step-3/index');
+});
+
+app.get('/post-card.html', function(req, res) {
+   res.render('polymer-tutorial-master/step-3/post-card.html');
+});
+
+app.get('/post-list.html', function(req, res) {
+   res.render('polymer-tutorial-master/step-3/post-list.html');
+});
+
+app.get('/aws-meme-generator', function(req, res) {
+   res.render('polymer-tutorial-master/aws-meme-generator/index.html');
+});
+
+app.get('/timer', function(req, res) {
+   res.render('polymer-tutorial-master/timer/index.html');
+});
+
 app.listen(3001);
